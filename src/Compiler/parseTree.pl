b732931deafb0d7f2a29a99a3ccb5061e9b04c95
prakharsam <prakharsam97@gmail.com>
2020-04-20 20:35:09 -0700

    boolean expression and declaration

--- src/Compiler/parseTree.pl
+++ src/Compiler/parseTree.pl
@@ -31,3 +31,37 @@ data_type-->[int].
 data_type-->[float].
 data_type-->[string].
 data_type-->[boolean].
+
+
+% Boolean Operators
+boolean_operator(t_bool_op_and(and))  --> [and].
+boolean_operator(t_bool_op_or(or))  --> [or].
+
+
+% Boolena Operations
+bool(false)-->[false].
+bool(true)--> [true].
+bool(t_notbool(not, X))--> [not], bool(X).
+bool(t_bool(X,Y,Z))--> expr(X), comparison_operator(Y), expr(Z).
+bool(t_bool_operation(X, Y, Z)) --> bool(X), boolean_operator(Y), bool(Z).
+
+
+% Need to redefine not equal to operator
+comparison_operator(t_comp_op(>)) --> [>].
+comparison_operator(t_comp_op(<)) --> [<].
+comparison_operator(t_comp_op(==)) --> [==].
+comparison_operator(t_comp_op(<=)) --> [<=].
+comparison_operator(t_comp_op(>=)) --> [>=].
+comparison_operator(t_comp_op('!=')) --> ['!='].
+
+
+% Ternary Operation
+ternary_op(t_ternary(X, Y, Z)) --> bool(X), [?], expr(Y), [:], expr(Z).
+
+% Declaration statements
+declaration(t_declaration_bool_assign(X, Y)) --> [boolean], identifier(X), [=], bool(Y).
+declaration(t_declaration_str_assign(X, Y)) --> [string], identifier(X), [=], ['"'], [Y], ['"'].
+declaration(t_declaration_num_assign(X, Y)) --> [int], identifier(X), [=], expr(Y).
+declaration(t_declaration_num_assign_ternary(X, Y)) --> [int], identifier(X), [=], ternary_op(Y).
+declaration(t_declaration_num_assign(X, Y)) --> [float], identifier(X), [=], expr(Y).
+declaration(t_declaration_num_assign_ternary(X, Y)) --> [float], identifier(X), [=], ternary_op(Y).
