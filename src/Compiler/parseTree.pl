92823d2312a26826ca2002f13318d3fa1e2b73e1
prakharsam <prakharsam97@gmail.com>
2020-04-20 20:34:02 -0700

    parse expression and data type

--- src/Compiler/parseTree.pl
+++ src/Compiler/parseTree.pl
@@ -1,1 +1,33 @@
 % Parse Tree Generator
+%:- use_rendering(svgtree).
+
+:- use_module(library(tabling)).
+:- table expr_op/3, term/3, bool/3.
+
+
+% Need to implement % operator and I = E too.
+% Expressions
+expr(t_assign(X, Y)) --> identifier(X), [=], expr_op(Y).
+expr(X) --> expr_op(X).
+
+expr_op(t_add(X, Y))-->expr_op(X), [+], term(Y).
+expr_op(t_sub(X, Y))-->expr_op(X), [-], term(Y).
+expr_op(X) --> term(X).
+
+term(t_div(X, Y))-->term(X), [/], brackets(Y).
+term(t_mul(X, Y)) --> term(X), [*], brackets(Y).
+term(X) --> brackets(X).
+
+brackets(X) --> ['('], expr(X), [')'].
+brackets(X) --> num(X).
+brackets(X) --> identifier(X).
+
+identifier(t_id(X)) -->[X],{X \= true}, {X \= false}, {atom(X)}.
+num(t_num(X)) --> [X], {number(X)}.
+
+
+% 4 data types
+data_type-->[int].
+data_type-->[float].
+data_type-->[string].
+data_type-->[boolean].
