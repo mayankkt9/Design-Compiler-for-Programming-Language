408d3a6f0a31d45c826bfad675a20cd204813a55
prakharsam <prakharsam97@gmail.com>
2020-04-20 20:51:35 -0700

    main program for parse tree and semantics

---
+++ src/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from pyswip import Prolog
+from compiler.lexer import lexer_job,set_up_env
+
+def create_parse_tree(lex):
+
+    prolog.consult("Complier/parseTree.pl")
+
+    query = "program(P,{},[])."
+
+    parsetree = prolog.query(query.format(lex))
+
+    return next(parsetree)["P"]
+
+def give_semantics(parse_tree):
+    prolog.consult("Runtime/runtime.pl")
+    query = "program_eval({},Z)"
+
+    Env = prolog.query(query.format(parse_tree))
+
+    print(next(Env)["Z"])
+
+
+# set_up_env()
+prolog = Prolog()
+lex = lexer_job()
+parse_tree = create_parse_tree(lex)
+give_semantics(parse_tree)
