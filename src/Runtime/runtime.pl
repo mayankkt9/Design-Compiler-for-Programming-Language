749308d0fa7b57b6f6715cce3b5a2074a001debb
Raghavan SS <rsampige@asu.edu>
2020-04-20 20:22:54 -0700

    Eval expr. Lookup and update

--- src/Runtime/runtime.pl
+++ src/Runtime/runtime.pl
@@ -1,1 +1,57 @@
 % Runtime Semantics
+
+% Update Environment
+update(t_id(K), V, Type, Env, FinalEnv) :- update(K, V, Type, Env, FinalEnv).
+update(K, V, Type, [], [(K, V, Type)]).
+
+update(K, V, Type, [(K, _, _)|T], [(K, V, Type)|T]).
+
+update(K, V, Type, [H|T], [H|R]) :- H \= (K,_,_), update(K, V, Type, T, R).
+
+
+% Lookup Value in Environment
+lookup(t_id(K), Env, V, Type) :- lookup(K, Env, V, Type).
+lookup(K, [(K,V,Type)|_], V, Type).
+
+lookup(K, [_|T], V, Type) :- lookup(K, T, V, Type).
+
+
+% Check if an identifier is present in env.
+check_present(t_id(K),Env) :- check_present(K, Env).
+check_present(K,[(K,_,_)|_]).
+check_present(K,[(H,_,_)|T]) :- K \= H, check_present(K,T).
+
+% Evaluate Expression
+eval_expr(t_assign(t_id(X), Y), Env, FinalEnv, Val):- check_present(X, Env),
+    eval_expr(Y, Env, Env1, Val), update(X, Val, num, Env1, FinalEnv).
+
+eval_expr(t_assign(t_id(X), _Y), Env, _FinalEnv, _Val):- \+check_present(X, Env),
+    write("Variable not initialised. Please check."), fail.
+
+eval_expr(t_assign(t_id(X), _Y), Env, _FinalEnv, _Val):- lookup(X, Env, _, Type),
+    Type \= num, write("This operation can only be perfomed on num type of variable. Please check."), fail.
+
+eval_expr(t_add(X, Y), Env, FinalEnv, Val):- eval_expr(X, Env, Env1, V1),
+                                             eval_expr(Y, Env1, FinalEnv, V2),
+                                             Val is V1 + V2.
+
+eval_expr(t_sub(X, Y), Env, FinalEnv, Val):- eval_expr(X, Env, Env1, V1),
+                                             eval_expr(Y, Env1, FinalEnv, V2),
+                                             Val is V1 - V2.
+
+eval_expr(t_div(X, Y), Env, FinalEnv, Val):- eval_expr(X, Env, Env1, V1),
+                                             eval_expr(Y, Env1, FinalEnv, V2),
+    							   			 Val is V1 / V2.
+
+eval_expr(t_mul(X, Y), Env, FinalEnv, Val):- eval_expr(X, Env, Env1, V1),
+                                             eval_expr(Y, Env1, FinalEnv, V2),
+    							   			 Val is V1 * V2.
+
+eval_expr(t_num(X), Env, Env, X).
+
+eval_expr(t_id(X), Env, Env, Val):- check_present(X, Env), lookup(X, Env, Val, num).
+
+eval_expr(t_id(X), Env, Env, _Val):- \+check_present(X, Env), write("Variable not initialised. Please check."), fail.
+
+eval_expr(t_id(X), Env, Env, Val):- lookup(X, Env, Val, Type), Type \= num,
+    write("This operation can only be perfomed on num type of variable. Please check."), fail.
