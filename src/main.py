5c0bbdb4477bd260a15637356cb622d54a304ad5
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-04-23 18:08:55 -0700

    Commented and Formatted the code.

--- src/main.py
+++ src/main.py
@@ -1,40 +1,73 @@
 #!/usr/bin/env python3
 from pyswip import Prolog
-from Compiler.lexer import lexer_job,set_up_env,lexer
+from Compiler.lexer import lexer
 import sys
+
+
 def create_parse_tree(lex):
+    '''
+    This method will take tokens from the lexer and generate parse tree
+
+    Arguments : 
+        lex : 
+            type : str
+            desc : list of tokens
+    Return : 
+        type : str
+        desc : parse tree generated after parsin the tokens
+    '''
 
     prolog.consult("Compiler/parseTree.pl")
-
     query = "program(P,{},[])."
-
     parsetree = prolog.query(query.format(lex))
-
     x = next(parsetree)["P"]
-    y = x.replace("(b'","('")
-
+    y = x.replace("b'", "'")
     return y
 
+
 def give_semantics(parse_tree):
+    '''
+    This method will take parse tree as input and execute code
+
+    Arguments : 
+        parse_tree : 
+            type : str
+            desc : The input parse tree generated after parsing the code.
+    Return Type : None
+    '''
+
     prolog.consult("Runtime/runtime.pl")
     query = "program_eval({},Z)"
-
     Env = prolog.query(query.format(parse_tree))
-    # print(list(Env))
     print(next(Env)["Z"])
 
-def get_arg():
-	file = ""
-	try:
-		file = sys.argv[1]
-	except:
-		print("Error \nScript Usage -> python3 main.py inputfile")
-		sys.exit(2)
-	return file
 
-set_up_env()
-prolog = Prolog()
-file = get_arg()
-lex = lexer(file)
-parse_tree = create_parse_tree(lex)
-give_semantics(parse_tree)
+def get_arg():
+    '''
+    This method will get system arguments to run the code.
+    The system argument here would be the input code in .rch format
+
+    Arguments : None
+    Return Type : file
+    '''
+
+    file = ""
+    try:
+        file = sys.argv[1]
+    except:
+        print("Error \nScript Usage -> python3 main.py inputfile")
+        sys.exit(2)
+    return file
+
+
+if __name__ == "__main__":
+
+    prolog = Prolog()
+    file = get_arg()
+    lex = lexer(file)
+    try:
+        parse_tree = create_parse_tree(lex)
+    except StopIteration:
+        print("Syntax Error")
+        sys.exit(0)
+    give_semantics(parse_tree)
